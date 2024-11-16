9d865021d717034cea4632ca357894bc7943019e
Feeras <feeras@gmx.de>
2024-11-16 18:50:15 +0100

    Add test setup

--- src/test/java/com/feeras/morgensonne/MorgensonneApplicationTests.java
+++ src/test/java/com/feeras/morgensonne/MorgensonneApplicationTests.java
@@ -1,10 +1,22 @@
 package com.feeras.morgensonne;
 
+import com.feeras.morgensonne.cron.ScheduledTasks;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MorgensonneApplicationTests {
+
+    @MockBean
+    ScheduledTasks tasks;
+
+    static {
+        System.setProperty("username", "foo");
+        System.setProperty("password", "bar");
+    }
 
     @Test
     void contextLoads() {
